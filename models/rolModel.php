132d705c4dbf3a4c5c83eb07789f3e0fffa53dc0
Segundo <segundogaldames@gmail.com>
2023-05-03 16:02:27 -0400

    Implementacion de modelo Rol

---
+++ models/rolModel.php
@@ -0,0 +1,57 @@
+<?php
+
+class rolModel extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getRoles()
+    {
+        $roles = $this->_db->query("SELECT id, nombre FROM roles ORDER BY nombre");
+        return $roles->fetchall();
+    }
+
+    public function getRolId($id)
+    {
+        $id = (int) $id;
+        $rol = $this->_db->prepare("SELECT id, nombre, created_at, updated_at FROM roles WHERE id = ?");
+        $rol->bindParam(1, $id);
+        $rol->execute();
+
+        return $rol->fetch();
+    }
+
+    public function getRolNombre($nombre)
+    {
+        $rol = $this->_db->prepare("SELECT id FROM roles WHERE nombre = ?");
+        $rol->bindParam(1, $nombre);
+        $rol->execute();
+
+        return $rol->fetch();
+    }
+
+    public function addRol($nombre)
+    {
+        $rol = $this->_db->prepare("INSERT INTO roles(nombre, created_at, updated_at) VALUES(?, now(), now())");
+        $rol->bindParam(1, $nombre);
+        $rol->execute();
+
+        $row = $rol->rowCount();
+        return $row;
+    }
+
+    public function editRol($id, $nombre)
+    {
+        $id = (int) $id;
+
+        $rol = $this->_db->prepare("UPDATE roles SET nombre = ?, updated_at = now() WHERE id = ?");
+        $rol->bindParam(1, $nombre);
+        $rol->bindParam(2, $id);
+        $rol->execute();
+
+        $row = $rol->rowCount();
+        return $row;
+    }
+}
